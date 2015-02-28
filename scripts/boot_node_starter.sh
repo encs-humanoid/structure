5dd37b5c50295cbd692233a47021c97a641527bf
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2015-02-28 22:40:30 +0000

    added command to set working directory

--- scripts/boot_node_starter.sh
+++ scripts/boot_node_starter.sh
@@ -8,4 +8,5 @@ export ROS_MASTER_URI=http://192.168.8.100:11311/
 export ROS_IP=`hostname --all-ip-addresses | xargs`
 
 # Installer script should set next line to path of boot_node.
+cd $SCRIPT_DIR
 $SCRIPT_DIR/boot_node.py > $SCRIPT_DIR/boot_node.log 2>&1
