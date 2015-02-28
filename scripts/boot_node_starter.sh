fe29f637019ae2e0122aa8800a7a3c378a39a29c
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2015-02-28 22:15:38 +0000

    attempt to get boot node working on startup

---
+++ scripts/boot_node_starter.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=/home/pi/catkin_ws/src/structure/scripts
+
+source /opt/ros/groovy/setup.bash
+source /home/pi/catkin_ws/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.8.100:11311/
+export ROS_IP=`hostname --all-ip-addresses | xargs`
+
+# Installer script should set next line to path of boot_node.
+$SCRIPT_DIR/boot_node.py > $SCRIPT_DIR/boot_node.log 2>&1
