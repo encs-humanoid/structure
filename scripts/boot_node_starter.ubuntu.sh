23ab68fa7decb26138bf05567673f8fa10aefac3
Mark Whelan <m@townunderground.net>
2015-02-28 18:36:26 -0500

    ubuntu starter sh and init file

---
+++ scripts/boot_node_starter.ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=/home/human/catkin_ws/src/structure/scripts
+
+source /opt/ros/hydro/setup.bash
+source /home/human/catkin_ws/devel/setup.bash
+export ROS_MASTER_URI=http://192.168.8.100:11311/
+export ROS_IP=`/bin/hostname --all-ip-addresses | /usr/bin/xargs`
+
+# Installer script should set next line to path of boot_node.
+cd $SCRIPT_DIR
+$SCRIPT_DIR/boot_node.py > $SCRIPT_DIR/boot_node.log 2>&1
